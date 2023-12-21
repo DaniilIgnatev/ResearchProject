cd3f937af0ac1c0ed499fc2e765bec171b16213e
Daniil <Ignatev>
2023-12-21 15:58:44 +0100

    bitstream checked

--- Vivado_Projects/SK9822_ZTurn_30x2LEDs/SK9822_ZTurn.gen/sources_1/bd/design_main/hdl/design_main_wrapper.v
+++ Vivado_Projects/SK9822_ZTurn_30x2LEDs/SK9822_ZTurn.gen/sources_1/bd/design_main/hdl/design_main_wrapper.v
@@ -2,7 +2,7 @@
 //Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2023.2 (win64) Build 4029153 Fri Oct 13 20:14:34 MDT 2023
-//Date        : Wed Dec 20 13:42:25 2023
+//Date        : Thu Dec 21 15:34:40 2023
 //Host        : ISCN5CG3184Y0Y running 64-bit major release  (build 9200)
 //Command     : generate_target design_main_wrapper.bd
 //Design      : design_main_wrapper
