f29b657fad19688a16c728159511c5d5023b7bc1
Daniil Ignatev <folkestone1726@gmail.com>
2024-01-04 22:46:01 +0400

    removed link to missing file

--- Vivado_Projects/SK9822_ZTurn_30x2LEDs/SK9822_ZTurn.gen/sources_1/bd/design_main/hdl/design_main_wrapper.v
+++ Vivado_Projects/SK9822_ZTurn_30x2LEDs/SK9822_ZTurn.gen/sources_1/bd/design_main/hdl/design_main_wrapper.v
@@ -2,8 +2,8 @@
 //Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2023.2 (win64) Build 4029153 Fri Oct 13 20:14:34 MDT 2023
-//Date        : Fri Dec 22 22:27:38 2023
-//Host        : Daniil-Nuc running 64-bit major release  (build 9200)
+//Date        : Thu Jan  4 22:42:27 2024
+//Host        : DaniilSP9 running 64-bit major release  (build 9200)
 //Command     : generate_target design_main_wrapper.bd
 //Design      : design_main_wrapper
 //Purpose     : IP block netlist
