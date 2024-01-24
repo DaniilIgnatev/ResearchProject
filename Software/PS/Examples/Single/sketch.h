86c6fd6035a63339d29992a423a5e5ce7bfe15f2
Daniil Ignatev <folkestone1726@gmail.com>
2024-01-24 20:15:07 +0100

    single sketch tested

--- Software/PS/Examples/Single/sketch.h
+++ Software/PS/Examples/Single/sketch.h
@@ -9,16 +9,27 @@
 #include "xparameters.h"
 #include "SK9822_AXI4.h"
 
-// First byte address of settings interface (CSR address)
-#define SK9822_AXI4_SETTINGS_BASEADDR 0x43C00000
-// First byte address of LED interface (first LED address)
-#define SK9822_AXI4_LED_BASEADDR 0x43C10000
-// First byte address of R interface
-#define SK9822_AXI4_R_BASEADDR 0x43C20000
-// First byte address of G interface
-#define SK9822_AXI4_G_BASEADDR 0x43C20004
-// First byte address of B interface
-#define SK9822_AXI4_B_BASEADDR 0x43C20008
+// First byte address of settings interface (CSR address) #1
+#define SK9822_AXI4_SETTINGS_BASEADDR_1 0x43C00000
+// First byte address of LED interface (first LED address) #1
+#define SK9822_AXI4_LED_BASEADDR_1 0x43C10000
+// First byte address of R interface #1
+#define SK9822_AXI4_R_BASEADDR_1 0x43C20000
+// First byte address of G interface #1
+#define SK9822_AXI4_G_BASEADDR_1 0x43C20004
+// First byte address of B interface #1
+#define SK9822_AXI4_B_BASEADDR_1 0x43C20008
+
+// First byte address of settings interface (CSR address) #2
+#define SK9822_AXI4_SETTINGS_BASEADDR_2 0x43C30000
+// First byte address of LED interface (first LED address) #2
+#define SK9822_AXI4_LED_BASEADDR_2 0x43C40000
+// First byte address of R interface #2
+#define SK9822_AXI4_R_BASEADDR_2 0x43C50000
+// First byte address of G interface #2
+#define SK9822_AXI4_G_BASEADDR_2 0x43C50004
+// First byte address of B interface #2
+#define SK9822_AXI4_B_BASEADDR_2 0x43C50008
 
 void setup();
 
